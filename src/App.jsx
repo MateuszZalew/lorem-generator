78d4b0bf286440b009a94014fdef6c8bd6d57819
zalesiu <zalewmat31@gmail.com>
2023-09-12 23:52:25 +0200

    add basic structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,36 @@
+import { useState } from "react";
+import dataText from "./data";
+
 const App = () => {
-  return <h2>Lorem Ipsum Starter</h2>;
+  const [number, setNumber] = useState(1);
+  const [text, setText] = useState([]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  const handleChange = (e) => {
+    setNumber(e.target.value);
+  };
+
+  return (
+    <section class="section-center">
+      <h4>Tired of boring lorem ipsum?</h4>
+      <form class="lorem-form" onSubmit={handleSubmit}>
+        <label for="number">Paragraphs:</label>
+        <input
+          type="number"
+          id="number"
+          value={number}
+          min={1}
+          max={8}
+          onChange={handleChange}
+        />
+        <button class="btn" type="submit">
+          Generate
+        </button>
+      </form>
+    </section>
+  );
 };
 export default App;
